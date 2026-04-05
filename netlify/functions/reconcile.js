8ad4c8f5ed3a8559b3cddec06f89c4296af889ca
andreagiacomoneri <andreagiacomoneri@gmail.com>
2026-04-05 17:59:33 +0200

    Delete netlify/functions/reconcile.js

--- netlify/functions/reconcile.js
+++
@@ -1,5 +0,0 @@
-export const config = { schedule: "0 0 * * *" }; // midnight every day
-export async function handler() {
-  await fetch(`${process.env.URL}/.netlify/functions/proxy?target=reconcile`);
-  return { statusCode: 200 };
-}
